720f7bf902b2adaab48cb6ff4ac865fb41db80bb
Lionel ADANKON <lionelad006@gmail.com>
2023-05-22 03:29:22 +0100

    Like and Dislike

--- backend/Controllers/post.controller.js
+++ backend/Controllers/post.controller.js
@@ -39,3 +39,27 @@ module.exports.deletePost = async (req, res) => {
     await post.deleteOne();
     res.status(200).json("Message supprimé " + req.params.id)
 }
+
+module.exports.likePost = async (req, res) => {
+    try {
+        await PostModel.findByIdAndUpdate(
+            req.params.id,
+            { $addToSet: { likers: req.body.userId } },
+            { new: true }
+        ).then((data) => res.status(200).send(data))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+module.exports.dislikePost = async (req, res) => {
+    try {
+        await PostModel.findByIdAndUpdate(
+            req.params.id,
+            { $pull: { likers: req.body.userId } },
+            { new: true }
+        ).then((data) => res.status(200).send(data))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { setPosts, getPosts, editPost, deletePost } = require("../Controllers/post.controller");
+const { setPosts, getPosts, editPost, deletePost, likePost, dislikePost } = require("../Controllers/post.controller");
 const router = express.Router();
 
 router.get("/", getPosts);
@@ -8,14 +8,10 @@ router.post("/", setPosts);
 
 router.put('/:id', editPost);
 
-router.delete('/:id', deletePost)
+router.delete('/:id', deletePost);
 
-router.patch("/like-post/:id", (req, res) => {
-    res.json({ message: "Post liké : id: " + req.params.id })
-})
+router.patch("/like-post/:id", likePost);
 
-router.patch("/dislike-post/:id", (req, res) => {
-    res.json({ message: "Post liké : id: " + req.params.id })
-})
+router.patch("/dislike-post/:id", dislikePost);
 
 module.exports = router;
